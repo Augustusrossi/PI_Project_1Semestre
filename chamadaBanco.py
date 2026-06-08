1d1492eb2b86cf6701f701a4b09d866478212c67
Augustus100 <augustusrossi14@gmail.com>
2026-06-08 20:02:56 -0300

    feat: mudança para conexão local

--- chamadaBanco.py
+++ chamadaBanco.py
@@ -2,25 +2,25 @@
 import mysql.connector
 
 
+# def obtem_conexao ():
+#     if obtem_conexao.conexao == None:
+#         obtem_conexao.conexao = mysql.connector.connect( 
+#             host=f"{'172.16.12.14'}",\
+#             user=f"{'BD240226135'}",\
+#             password=f"{'Dkvwx7'}",\
+#             database=f"{'BD240226135'}"
+#         )
+#     return obtem_conexao.conexao
+
+
 def obtem_conexao ():
     if obtem_conexao.conexao == None:
-        obtem_conexao.conexao = mysql.connector.connect( 
-            host=f"{'172.16.12.14'}",\
-            user=f"{'BD240226135'}",\
-            password=f"{'Dkvwx7'}",\
-            database=f"{'BD240226135'}"
+       obtem_conexao.conexao = mysql.connector.connect( 
+            host=f"{'127.0.0.1'}",\
+            user=f"{'root'}",\
+            password=f"{'123456'}",\
+            database=f"{'sistemahospital'}"
         )
     return obtem_conexao.conexao
-
-
-#def obtem_conexao ():
-#    if obtem_conexao.conexao == None:
-#       obtem_conexao.conexao = mysql.connector.connect( 
-#            host=f"{'127.0.0.1'}",\
-#            user=f"{'root'}",\
-#            password=f"{'123456'}",\
-#            database=f"{'sistemahospital'}"
-#        )
-#    return obtem_conexao.conexao
 
 obtem_conexao.conexao = None
